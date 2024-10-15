b19c8fa6748429e609e9b146a0f74324051a9406
DanPaul12 <dan.paul.schechter@gmail.com>
2024-10-15 10:25:56 -0700

    hw47

--- searchpage.js
+++ searchpage.js
@@ -1,15 +1,30 @@
-
-document.getElementById("form").addEventListener("submit", function(event){
+/*document.getElementById("mybutton").addEventListener("click", async(event)=>{
     event.preventDefault()
-    const input = document.getElementById("pokesearch")
-    const Pokename = input.value
-    console.log(Pokename)
-})
-
-
-
-/*async function getPokemon(Pokename){
+    const Pokename = document.getElementById("pokesearch").value
+    console.log(`${Pokename}`)
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${Pokename}`)
     const pokedata = await response.json()
     console.log(pokedata)
-}*/
+})*/
+
+
+
+
+async function getPokemon(Pokename){
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${Pokename}`)
+    const pokedata = await response.json()
+    return pokedata    
+}
+
+document.getElementById("mybutton").addEventListener("click", async(event) => {
+    event.preventDefault()
+    const newpoke = await document.getElementById("pokesearch").value
+    const newpokedata = await getPokemon(newpoke)
+    const pictureElement = document.getElementById("main1")
+    const infoElement = document.getElementById("main2")
+    pictureElement.innerHTML = `<img src="${newpokedata.sprites.front_default}" alt="${newpokedata.name}">`
+    infoElement.innerHTML = `<h2>${newpokedata.name}</h2>
+                            <h4>${newpokedata.base_experience}</h4>
+                            <img src="${newpokedata.sprites.back_default}" alt="${newpokedata.name}">`
+    console.log(`${newpokedata.name}`)
+})
