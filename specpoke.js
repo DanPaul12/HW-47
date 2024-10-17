20fdf197c747493d38dd0ced336f7980133e8892
DanPaul12 <dan.paul.schechter@gmail.com>
2024-10-16 23:43:28 -0700

    hw47

--- specpoke.js
+++ specpoke.js
@@ -9,7 +9,12 @@ document.addEventListener("DOMContentLoaded", async()=>{
     const picElement = document.getElementById("main1")
     const infoElement = document.getElementById("main2")
     picElement.innerHTML = `<img src= ${specdata.sprites.other.home.front_default}>`
+                            `<img src= ${specdata.sprites.other.official-artwork.front_default}>`
     infoElement.innerHTML = `<h2>Name: ${specdata.name}</h2>
+                            <h4>Moves:</h4>
+                            <ul>
+                                ${specdata.moves.map(move => `<li>${move.move.name}</li>`).join('')}
+                            </ul>
                             <h4>Abilities:</h4>
                             <ul>
                                 <li>${specdata.abilities[0].ability.name}</li>
